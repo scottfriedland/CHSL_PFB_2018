76892820636b5512bee670e16f1e8e83ca3ab063
sfr <sfr@pfb14.cshl.edu>
2018-10-16 11:03:16 -0400

    Saving my first script

---
+++ myFirstPythonScript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+## A program to print out my name
+import sys
+
+## Taking arguments from the command line
+my_name = sys.argv[1]  #my name as the first parameter
+
+# printing the outputs to the command line
+print('My name is: ', my_name)
+
