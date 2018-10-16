30c7e83ae167dc8d4a8190aee3d29ac1ffc58238
sfr <sfr@pfb14.cshl.edu>
2018-10-16 11:15:18 -0400

    Updated First Script

--- myFirstPythonScript.py
+++ myFirstPythonScript.py
@@ -4,8 +4,13 @@
 import sys
 
 ## Taking arguments from the command line
-my_name = sys.argv[1]  #my name as the first parameter
-
+my_name = sys.argv[1]  # my name as the first parameter
+my_favorite_color = sys.argv[2] # my favorite color is second parameter 
+my_favorite_activity = sys.argv[3] # my fac activity is third input
+my_fav_animal = sys.argv[4] # my fav animal is 4th input  
 # printing the outputs to the command line
-print('My name is: ', my_name)
+print('My name is:', my_name)
+print('My favorite color:',my_favorite_color)
+print('My favorite activity:', my_favorite_activity)
+print('My favorite animal:', my_fav_animal)
 
